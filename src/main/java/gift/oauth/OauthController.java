d7a87286d9affebf893cf5dfee7128aeed4e2d8e
ensalada.de.pollo <ensalada.de.pollo@90.local>
2025-08-01 15:36:38 +0900

    fix: 카카오 로그인 요청 api에서 scope를 추가하여 메세지 전송이 되지 않는 문제 해결

--- src/main/java/gift/oauth/OauthController.java
+++ src/main/java/gift/oauth/OauthController.java
@@ -4,7 +4,6 @@ import gift.common.exceptions.LogInFailedException;
 import gift.common.property.KakaoProperties;
 import gift.jwt.JwtResponse;
 import gift.oauth.dto.KakaoLoginRequest;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,6 +27,7 @@ public class OauthController {
     public void redirectToKakao(HttpServletResponse response) {
         String kakaoUrl = UriComponentsBuilder
             .fromUriString(kakaoProperties.kakaoAuthorizeUri())
+            .queryParam("scope", "talk_message,account_email")
             .queryParam("response_type", "code")
             .queryParam("client_id", kakaoProperties.clientId())
             .queryParam("redirect_uri", kakaoProperties.redirectUri())
@@ -44,15 +44,8 @@ public class OauthController {
     public void login(
         @ModelAttribute KakaoLoginRequest kakaoLoginRequest, HttpServletResponse response) {
         JwtResponse jwt = kakaoAuthService.login(kakaoLoginRequest);
-
-        Cookie cookie = new Cookie("token", jwt.accessToken());
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(60);
-        response.addCookie(cookie);
-
         try {
-            response.sendRedirect("/");
+            response.sendRedirect("/?token=" + jwt.accessToken());
         } catch (Exception e) {
             throw new LogInFailedException(e.getMessage());
         }
